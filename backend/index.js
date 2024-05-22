dbc7369c6ab5b44cf03de4d9c36fbd4b529402f7
Lescillei <lescillei.dorneles@sempreceub.com>
2024-05-22 10:23:59 -0300

    adicionando index.js

---
+++ backend/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+//const com instancia da biblioteca express
+const aplicacao = express();
+const port = 4000;
+
+aplicacao.get('/', (req, res) => {
+    res.send("Chamei o backend com sucesso");
+});
+
+aplicacao.post('/', (req,res) => {
+    res.send("Chamei back com sucesso usando post");
+});
+
+//Criando os endpoints
+aplicacao.get('/moedas', (req,res) => {
+    res.send("Compatível com Real, Dólar e Euro");
+
+    const moedas = {
+        BRL: "real",
+        USD: "dólar",
+        EUR: "euro"
+    }
+});
+
+aplicacao.get('/info', (req,res) => {
+    res.send("Informações sobre o sistema")
+});
+
+aplicacao.get('/disponiveis', (req,res) => {
+    res.send()
+});
+
+
+//Aplicação ouvindo a porta 4000
+aplicacao.listen(port, () => {
+    console.log("Escutando na porta 4000");
+});
+
+//Ao rodar "node .\index.js" o servidor começa
+//ao acessar url "http://localhost:4000" acessa ele
+//Use POSTMAN para utilizar outros métodos HTTP
+
